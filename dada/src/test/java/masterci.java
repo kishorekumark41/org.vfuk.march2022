4bb1cf23b6b36e5f8050138b463b9618d06dc3fe
kishorekumark41 <kishorekumark41@gmail.com>
2022-03-27 14:36:06 +0530

    1877 changes in master ci

--- dada/src/test/java/masterci.java
+++ dada/src/test/java/masterci.java
@@ -1,6 +1,10 @@
 
 public class masterci {
 
+    public void abc()
+    {
+        System.out.println("abc method");
+    }
     public static void main(String[] args) {
         System.out.println("masterci");
     }
